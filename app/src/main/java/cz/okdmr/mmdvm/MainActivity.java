cb4601620ead93243eb4bc66bf62f80c94361df5
Marek Sebera <marek.sebera@gmail.com>
2022-03-25 17:35:23 +0100

    initial project

---
+++ app/src/main/java/cz/okdmr/mmdvm/MainActivity.java
@@ -0,0 +1,14 @@
+package cz.okdmr.mmdvm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
