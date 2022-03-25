fb56f97ca088eb79a5cb580771108557005f55df
Marek Sebera <marek.sebera@gmail.com>
2022-03-25 20:04:24 +0100

    straight to vocoder

--- app/src/main/cpp/mmdvm.cpp
+++ app/src/main/cpp/mmdvm.cpp
@@ -1,1 +1,13 @@
 #include "mmdvm.h"
+
+extern "C"
+JNIEXPORT jint JNI_OnLoad(JavaVM* vm, void* reserved) {
+    return JNI_VERSION_1_6;
+}
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_cz_okdmr_mmdvm_MainActivity_getNativeString(JNIEnv *env, jobject thiz) {
+    BeOn::AmbeVocoder *voice = new BeOn::AmbeVocoder(0);
+    return env->NewStringUTF("Ahoj");
+}
--- app/src/main/java/cz/okdmr/mmdvm/MainActivity.java
+++ app/src/main/java/cz/okdmr/mmdvm/MainActivity.java
@@ -11,23 +11,17 @@ import com.harris.rf.bbptt.core.IBeOnEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
-    private IBeOnEventListener pttListener = new IBeOnEventListener() {
-        @Override
-        public void onBeOnEvent(int i, int i2, int i3, Object obj) {
-            Log.d("PttListener", String.format("onBeOnEvent %d %d %d", i, i2, i3));
-        }
-    };
+    native String getNativeString();
+
+    static {
+        System.loadLibrary("mmdvm");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView t = findViewById(R.id.hello);
-        t.setText("Čau");
-
-        BbPttCore core = new BbPttCore();
-        BbPttCore.setBbPttCoreLogLevel(0);
-        core.addListener(0, pttListener);
-        Log.d("MainActivity", core.isStarted() ? "STARTED" : "CLOSED");
+        t.setText(getNativeString());
     }
 }
