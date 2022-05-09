3681dacf4bdf1a5fef0d7b14572ea2327da578fb
Marek Sebera <marek.sebera@gmail.com>
2022-05-09 21:28:09 +0200

    saving work

--- app/src/main/java/cz/okdmr/mmdvm/MainActivity.java
+++ app/src/main/java/cz/okdmr/mmdvm/MainActivity.java
@@ -1,15 +1,9 @@
 package cz.okdmr.mmdvm;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
-import com.harris.rf.bbptt.core.BbPttCore;
-import com.harris.rf.bbptt.core.IBeOnEventListener;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
